219eed73aa7a36304d86d6100b8146b824978220
vierfuffzig <tunella@gmx.de>
2020-01-06 21:26:08 +0100

    fix var types and cleanup

--- LTM2MAVLink/LTM2MAVLink.ino
+++ LTM2MAVLink/LTM2MAVLink.ino
@@ -23,58 +23,50 @@
 #include <AltSoftSerial.h>
 
 //    CONFIGURE BAUDRATES
-//    LTM INPUT FIXED TO PIN 8 (ALTSOFTSERIAL RX)
-//    MAVLINK OUTPUT ON TX (HARDWARE SERIAL)
 
 #define baud_mavlink_out 19200
 #define baud_LTM_in      2400
 
-// Parameter setup
-// If the input protocol does not provide values for all of these, parameters will be left at their default values.
-// All parameters set here are passed onward through serial output in MAVLink format.
+//    LTM INPUT FIXED TO PIN 8 (ALTSOFTSERIAL RX)
+//    MAVLINK OUTPUT ON TX (HARDWARE SERIAL)
+//    OUTPUT BAUDRATES > 38400 MIGHT CAUSE PERFORMANCE ISSUES
 
-//Basic UAV Parameters
-uint8_t system_id = 1;        // MAVLink system ID. Leave at 0 unless you need a specific ID.
-uint8_t component_id = 0;     // Should be left at 0. Set to 190 to simulate mission planner sending a command
-uint8_t system_type = 1;      // UAV type. 0 = generic, 1 = fixed wing, 2 = quadcopter, 3 = helicopter
-uint8_t autopilot_type = 0;   // Autopilot type. Usually set to 0 for generic autopilot with all capabilities
-uint8_t system_mode = 64;     // Flight mode. 4 = auto mode, 8 = guided mode, 16 = stabilize mode, 64 = manual mode
-uint32_t custom_mode = 0;     // Usually set to 0          
-uint8_t system_state = 4;     // 0 = unknown, 3 = standby, 4 = active
-uint32_t upTime = 0;          // System uptime, usually set to 0 for cases where it doesn't matter
+//    MAVLink system parameters, see common.h for further detail
+uint8_t    system_id = 0;        // Leave at 0 unless you need a specific ID
+uint8_t    component_id = 1;     // Leave at 0 = all
+uint8_t    system_type = 1;      // 0 Generic, 1 Fixed wing, 2 Quadrotor, 3 Coax Helicopter, 4 Helicopter, 5 Ground installation, 6 GCS
+uint8_t    autopilot_type = 3;   // Leave at 0 for generic autopilot with all capabilities, 3 = ArduPilot
+uint8_t    system_mode = 128;    // Flight mode. 4 = auto mode, 8 = guided mode, 16 = stabilize mode, 64 = manual mode, 128 = ARMED
+uint32_t   custom_mode = 0;      // Leave at 0 = not used          
+uint8_t    system_state = 4;     // 0 = unknown, 3 = standby, 4 = active
+uint32_t   upTime = 0;           // Leave at 0 if not required otherwise
 
-//
-// Flight parameters
-int16_t roll = 0;         // Roll angle in degrees
-int16_t pitch = 0;        // Pitch angle in degrees
-int16_t heading = 0;    // Geographical heading angle in degrees
-int32_t lat = 0;        // GPS latitude in degrees (example: 47.123456)
-int32_t lon = 0;        // GPS longitude in degrees
-int32_t alt = 0;        // Relative flight altitude in m
-uint8_t groundspeed = 0; // Groundspeed in m/s
-uint8_t airspeed = 0;    // Airspeed in m/s
-
-// GPS parameters
-uint8_t gps_sats = 0;     // Number of visible GPS satellites
-uint8_t fixType = 0;      // GPS fix type. 0-1: no fix, 2: 2D fix, 3: 3D fix
-
-// Battery parameters
-uint16_t voltage_battery = 0;    // Battery voltage in V
-uint16_t current_battery = 0;    // Battery current in A
-uint16_t battery_consumed = 0;   // mAh used
-
-uint8_t rssi = 0;
-uint8_t armed = 0;
-uint8_t failsafe = 0;
-uint8_t mode = 0;
+//    Flight parameters
+int16_t    roll = 0;               // LTM [deg] -> MAVLink [rad]
+int16_t    pitch = 0;              // LTM [deg] -> MAVLink [rad]
+uint16_t   heading = 0;            // course over ground [degrees]
+int32_t    lat = 0;
+int32_t    lon = 0;
+int32_t    alt = 0;                // LTM [m]   -> MAVLink [cm]
+uint16_t   groundspeed = 0;        // LTM [m/s] -> MAVLink [cm/s]
+uint8_t    airspeed = 0;           // LTM [m/s] -> MAVLink [cm/s]
+uint8_t    gps_sats = 0;
+uint8_t    fixType = 0;            // 0-1: no fix, 2: 2D fix, 3: 3D fix
+uint16_t   voltage_battery = 0;    // [mV]
+uint16_t   current_battery = 0;    // [mA]
+// uint16_t   battery_consumed = 0;   // [mAh] not used
+uint8_t    rssi = 0;               // not used in MAVLink downlink
+uint8_t    armed = 0;              // not used in MAVLink downlink
+uint8_t    failsafe = 0;           // not used in MAVLink downlink
+uint8_t    mode = 0;               // not used in MAVLink downlink
 
 AltSoftSerial softSerial;
 
 void setup() {
     Serial.begin(baud_mavlink_out);
-    //Serial.println("This is hardware serial for mavlink output");
+    //Serial.println("PUT HARDWARE SERIAL DEBUG MESSAGE HERE");
     softSerial.begin(baud_LTM_in);
-    //softSerial.println("This is software serial for LTM input");
+    //softSerial.println("PUT SOFTWARE SERIAL DEBUG MESSAGE HERE");
 }
  
 // Main loop: read LTM and pack to MAVLink
@@ -85,7 +77,7 @@ void loop() {
     ltm_read();
     
     // Send MAVLink heartbeat
-    command_heartbeat(system_id, component_id, custom_mode, system_type, autopilot_type, system_mode, system_state);
+    command_heartbeat(system_id, component_id, system_type, autopilot_type, system_mode, custom_mode, system_state);
     
     //Send battery status
     command_status(system_id, component_id, voltage_battery, current_battery);
@@ -96,7 +88,6 @@ void loop() {
     
     // Send attitude data to artificial horizon
     command_attitude(system_id, component_id, upTime, roll, pitch);
-
 }  
 
 /* #################################################################################################################
@@ -121,7 +112,7 @@ void loop() {
 #define LIGHTTELEMETRY_START2 0x54 //T
 #define LIGHTTELEMETRY_GFRAME 0x47 //G GPS + Baro altitude data ( Lat, Lon, Groundspeed, Alt, Sats, Sat fix)
 #define LIGHTTELEMETRY_AFRAME 0x41 //A Attitude data ( Roll,Pitch, Heading )
-#define LIGHTTELEMETRY_SFRAME 0x53 //S Sensors/Status data ( VBat, Consumed current, Rssi, Airspeed, Arm status, Failsafe status, Flight mode )
+#define LIGHTTELEMETRY_SFRAME 0x53 //S Sensors/Status data ( voltage, current, Rssi, Airspeed, Arm status, Failsafe status, Flight mode )
 #define LIGHTTELEMETRY_GFRAMELENGTH 18
 #define LIGHTTELEMETRY_AFRAMELENGTH 10
 #define LIGHTTELEMETRY_SFRAMELENGTH 11
@@ -157,8 +148,8 @@ void ltm_check() {
     if (LTMcmd==LIGHTTELEMETRY_GFRAME) {
         lat = ltmread_u32();
         lon = ltmread_u32();
-        groundspeed = ltmread_u8(); 
-        alt = ltmread_u32();
+        groundspeed = (uint16_t)ltmread_u8(); 
+        alt = ltmread_u32() * 100;
         uint8_t ltm_satsfix = ltmread_u8();
         gps_sats = (ltm_satsfix >> 2) & 0xFF;
         fixType = ltm_satsfix & 0b00000011;
@@ -171,16 +162,16 @@ void ltm_check() {
     }
 
     if (LTMcmd==LIGHTTELEMETRY_SFRAME) {
-        static int frametick = 0;
+        //    static int frametick = 0;
         voltage_battery = ltmread_u16();
         current_battery = ltmread_u16();
         rssi = ltmread_u8();
         airspeed = ltmread_u8();
         uint8_t ltm_armfsmode = ltmread_u8();
-        armed = (bool)(ltm_armfsmode & 0b00000001);
+        armed = (ltm_armfsmode & 0b00000001);
         failsafe = (ltm_armfsmode >> 1) & 0b00000001;
         mode = (ltm_armfsmode >> 2) & 0b00111111;     
-        frametick = millis();
+        //   frametick = millis();
     }
 }
 
@@ -252,14 +243,14 @@ void ltm_read() {
 * @return void
 *************************************************************/
 
-void command_heartbeat(uint8_t system_id, uint8_t component_id, uint32_t custom_mode, uint8_t system_type, uint8_t autopilot_type, uint8_t system_mode, uint8_t system_state) {
+void command_heartbeat(uint8_t system_id, uint8_t component_id, uint8_t system_type, uint8_t autopilot_type, uint8_t system_mode, uint32_t custom_mode, uint8_t system_state) {
 
     // Initialize the required buffers
     mavlink_message_t msg;
     uint8_t buf[MAVLINK_MAX_PACKET_LEN];
  
     // Pack the message
-    mavlink_msg_heartbeat_pack(system_id, component_id, &msg, custom_mode, system_type, autopilot_type, system_mode, system_state);
+    mavlink_msg_heartbeat_pack(system_id, component_id, &msg, system_type, autopilot_type, system_mode, custom_mode, system_state);
  
     // Copy the message to the send buffer
     uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
@@ -282,7 +273,7 @@ void command_status(uint8_t system_id, uint8_t component_id, uint16_t voltage_ba
     uint8_t buf[MAVLINK_MAX_PACKET_LEN];
 
     // Pack the message
-    mavlink_msg_sys_status_pack(system_id, component_id, &msg, 32767, 32767, 32767, 500, voltage_battery, current_battery, 0, 0, 0, 0, 0, 0, -1);
+    mavlink_msg_sys_status_pack(system_id, component_id, &msg, 32767, 32767, 32767, 500, voltage_battery, current_battery, -1, 0, 0, 0, 0, 0, 0);
 
     // Copy the message to the send buffer
     uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
@@ -298,14 +289,14 @@ void command_status(uint8_t system_id, uint8_t component_id, uint16_t voltage_ba
 * @return void
 *************************************************************/
 
-void command_gps(int8_t system_id, int8_t component_id, uint32_t upTime, int32_t lat, int32_t lon, int32_t alt,  uint8_t groundspeed, int16_t heading, uint8_t fixType, uint8_t gps_sats) {
+void command_gps(uint8_t system_id, uint8_t component_id, uint32_t upTime,  uint8_t fixType, int32_t lat, int32_t lon, int32_t alt, uint16_t groundspeed, uint16_t heading, uint8_t gps_sats) {
 
     // Initialize the required buffers
     mavlink_message_t msg;
     uint8_t buf[MAVLINK_MAX_PACKET_LEN];
 
     // Pack the message
-    mavlink_msg_gps_raw_int_pack(system_id, component_id, &msg, upTime, fixType, lat, lon, alt, UINT16_MAX, UINT16_MAX, groundspeed, heading * 100, gps_sats);
+    mavlink_msg_gps_raw_int_pack(system_id, component_id, &msg, upTime, fixType, lat, lon, alt, 65535, 65535, groundspeed, heading * 100, gps_sats);
 
     // Copy the message to the send buffer
     uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
@@ -321,7 +312,7 @@ void command_gps(int8_t system_id, int8_t component_id, uint32_t upTime, int32_t
 * @return void
 *************************************************************/
        
-void command_attitude(int8_t system_id, int8_t component_id, int32_t upTime, int16_t roll, int16_t pitch) {
+void command_attitude(uint8_t system_id, uint8_t component_id, int32_t upTime, int16_t roll, int16_t pitch) {
 
     //Radian -> degree conversion rate
     float radian = 57.2958;
